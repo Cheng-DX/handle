758ec5efded0320f9b18164bd79165e1eb19e071
Cheng_DX <2337040896@qq.com>
2022-03-28 21:15:43 +0800

    feat: simple random answer

--- src/use/init.ts
+++ src/use/init.ts
@@ -1,7 +1,18 @@
 import { pinyin } from 'pinyin-pro'
+import { ref } from 'vue'
 
 function random() {
-  return '吉星高照'
+  const answerList = [
+    '吉星高照',
+    '班门弄斧',
+    '水落石出',
+    '巧夺天工',
+    '名震天下',
+    '武运昌隆',
+    '鬼斧神工',
+    '擅作主张'
+  ]
+  return answerList[Math.floor(Math.random() * answerList.length)]
 }
 
 export function init() {
@@ -12,8 +23,8 @@ export function init() {
     toneType: 'none'
   }).split(' ')
   const answerArr = [...answer]
-
   return {
+    answer,
     answerSheng,
     answerYun,
     answerArr
